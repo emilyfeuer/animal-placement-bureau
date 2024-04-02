db44daabf05e234cc88bc254e152f8f08a8ca7b9
Emily Feuer <emilymfeuer@gmail.com>
2024-04-02 17:39:48 -0400

    very basic navbar setup

--- apbsite/apbsite/settings.py
+++ apbsite/apbsite/settings.py
@@ -20,6 +20,8 @@ dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = "/var/www/example.com/static/"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
