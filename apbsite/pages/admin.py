0d7111801836058be033deaf43cbaa6e16f725d2
Matt Willemin <mwill3663@gmail.com>
2024-04-02 18:13:25 -0400

    fefefee

--- apbsite/pages/admin.py
+++ apbsite/pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class User(AbstractUser):
+  USER_TYPE_CHOICES = (
+      (1, 'full_admin'),
+      (2, 'limited_admin'),
+      (3, 'editor'),
+      
+  )
+
+  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
