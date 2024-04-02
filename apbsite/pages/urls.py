05fd818c9493ef53b14c7ce58e39f6c9a81e0840
Emily Feuer <emilymfeuer@gmail.com>
2024-04-02 00:35:36 -0400

    displaying database info in index

--- apbsite/pages/urls.py
+++ apbsite/pages/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+# urlpatterns = [
+#     path("", views.index, name="index"),
+# ]
+
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /pages/5/
+    path("<int:option_id>/", views.detail, name="detail"),
 ]
